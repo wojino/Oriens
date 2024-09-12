de2dcbc1fc0dd20daab72d4003a9ee1ec44fc8b2
Woojin Kim <woojinkim@unist.ac.kr>
2024-09-12 05:30:02 +0000

    timer

---
+++ oriens/utils.py
@@ -0,0 +1,11 @@
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} executed in {time.time() - start} seconds")
+        return result
+
+    return wrapper
