8e1d6ca2b68b036bfc2db1ad22cdff5ef34b6016
Woojin Kim <woojinkim@unist.ac.kr>
2024-09-12 05:52:00 +0000

    bag_manager returns cv2 image in dataframe

--- oriens/bag_manager.py
+++ oriens/bag_manager.py
@@ -1,5 +1,8 @@
 import rosbag
 import pandas as pd
+import sensor_msgs.msg
+import cv2
+from cv_bridge import CvBridge
 
 
 class BagManager:
@@ -14,28 +17,35 @@ class BagManager:
                 [
                     msg.header.stamp.secs,
                     msg.header.stamp.nsecs,
-                    msg.lat,
-                    msg.lon,
-                    msg.alt,
+                    msg.lat / 1e7,
+                    msg.lon / 1e7,
                 ]
             )
         self.gps_data = pd.DataFrame(
-            self.gps_data, columns=["sec", "nsec", "lat", "lon", "alt"]
+            self.gps_data, columns=["sec", "nsec", "lat", "lon"]
         )
         return self.gps_data
 
     def get_image_dataframe(self):
+        bridge = CvBridge()
+
         for _, msg, t in self.bag.read_messages(
             topics=["/zedx/zed_node/left/image_rect_color"]
         ):
+            img_msg = sensor_msgs.msg.Image()
+            img_msg.height = msg.height
+            img_msg.width = msg.width
+            img_msg.encoding = msg.encoding
+            img_msg.is_bigendian = msg.is_bigendian
+            img_msg.step = msg.step
+            img_msg.data = msg.data
+
+            img = bridge.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
+
             self.camera_data.append(
-                [
-                    msg.header.stamp.secs,
-                    msg.header.stamp.nsecs,
-                    msg.data,
-                ]
+                [msg.header.stamp.secs, msg.header.stamp.nsecs, img]
             )
         self.camera_data = pd.DataFrame(
-            self.camera_data, columns=["sec", "nsec", "data"]
+            self.camera_data, columns=["sec", "nsec", "img"]
         )
         return self.camera_data
