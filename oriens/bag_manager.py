7cd530f01a2d8c732f8ad71dc73049a2f94c048b
Woojin Kim <woojinkim@unist.ac.kr>
2024-09-12 05:29:37 +0000

    Use rosbag instead of bagpy

--- oriens/bag_manager.py
+++ oriens/bag_manager.py
@@ -1,12 +1,41 @@
-import bagpy
-from bagpy import bagreader
+import rosbag
 import pandas as pd
 
 
 class BagManager:
     def __init__(self, bag_file):
-        self.bag_reader = bagreader(bag_file)
-        self.topic_table = self.bag_reader.topic_table
+        self.bag = rosbag.Bag(bag_file)
+        self.gps_data = []
+        self.camera_data = []
 
-    def get_dataframe(self, topic):
-        return pd.read_csv(self.bag_reader.message_by_topic(topic))
+    def get_gps_dataframe(self):
+        for _, msg, t in self.bag.read_messages(topics=["/mavros/sdcstatus/gpsrawint"]):
+            self.gps_data.append(
+                [
+                    msg.header.stamp.secs,
+                    msg.header.stamp.nsecs,
+                    msg.lat,
+                    msg.lon,
+                    msg.alt,
+                ]
+            )
+        self.gps_data = pd.DataFrame(
+            self.gps_data, columns=["sec", "nsec", "lat", "lon", "alt"]
+        )
+        return self.gps_data
+
+    def get_image_dataframe(self):
+        for _, msg, t in self.bag.read_messages(
+            topics=["/zedx/zed_node/left/image_rect_color"]
+        ):
+            self.camera_data.append(
+                [
+                    msg.header.stamp.secs,
+                    msg.header.stamp.nsecs,
+                    msg.data,
+                ]
+            )
+        self.camera_data = pd.DataFrame(
+            self.camera_data, columns=["sec", "nsec", "data"]
+        )
+        return self.camera_data
