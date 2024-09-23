c2e262810b9ae30e7efec79eeb4bc31c21b3df2f
Woojin Kim <woojinkim@unist.ac.kr>
2024-09-23 04:05:22 +0000

    Change focal length (optimization)

--- oriens/localizer.py
+++ oriens/localizer.py
@@ -13,7 +13,7 @@ class Localizer:
         self,
         image,
         prior_latlon,
-        focal_length=None,
+        focal_length=368,
         tile_size_meters=64,
         num_rotations=256,
         device="cuda",
@@ -52,50 +52,5 @@ class Localizer:
         )
 
         latlon = proj.unproject(uv)
-
-        # from oriens.maploc.osm.viz import GeoPlotter
-        # from oriens.maploc.osm.viz import Colormap, plot_nodes
-        # from oriens.maploc.utils.viz_2d import plot_images
-
-        # plot = GeoPlotter(zoom=16)
-        # plot.points(proj.latlonalt[:2], "red", name="location prior", size=10)
-        # plot.bbox(proj.unproject(bbox), "blue", name="map tile")
-        # plot.fig.show()
-
-        # tiler = TileManager.from_bbox(
-        #     proj, bbox + 10, self.demo.config.data.pixel_per_meter
-        # )
-        # map_viz = Colormap.apply(canvas.raster)
-        # plot_images([image, map_viz], titles=["input image", "OpenStreetMap raster"])
-        # plot_nodes(1, canvas.raster[2], fontsize=6, size=10)
-
-        # from oriens.maploc.utils.viz_localization import (
-        #     likelihood_overlay,
-        #     plot_dense_rotations,
-        #     add_circle_inset,
-        # )
-        # from oriens.maploc.utils.viz_2d import features_to_RGB
-
-        # # Visualize the predictions
-        # overlay = likelihood_overlay(
-        #     prob.numpy().max(-1), map_viz.mean(-1, keepdims=True)
-        # )
-        # (neural_map_rgb,) = features_to_RGB(neural_map.numpy())
-        # plot_images([overlay, neural_map_rgb], titles=["prediction", "neural map"])
-        # ax = plt.gcf().axes[0]
-        # ax.scatter(*canvas.to_uv(bbox.center), s=5, c="red")
-        # plot_dense_rotations(ax, prob, w=0.005, s=1 / 25)
-        # add_circle_inset(ax, uv)
-        # plt.savefig("oriens/experiments/localization.png", dpi=300, bbox_inches="tight")
-
-        # # Plot as interactive figure
-        # bbox_latlon = proj.unproject(canvas.bbox)
-        # plot = GeoPlotter(zoom=16.5)
-        # plot.raster(map_viz, bbox_latlon, opacity=0.5)
-        # plot.raster(likelihood_overlay(prob.numpy().max(-1)), proj.unproject(bbox))
-        # plot.points(proj.latlonalt[:2], "red", name="location prior", size=10)
-        # plot.points(proj.unproject(canvas.to_xy(uv)), "black", name="argmax", size=10)
-        # plot.bbox(bbox_latlon, "blue", name="map tile")
-        # plot.fig.show()
 
         return latlon, yaw
